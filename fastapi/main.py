054e4b8c66bc7ecaa6c03e1ef3a2fe4e057411a1
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-09-21 10:08:26 +0700

    🔥 Use urllib instead of requests

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import requests
+from urllib import request
+
 import uvicorn
 import py_eureka_client.eureka_client as eureka_client
+import json
 
 app = FastAPI()
 
@@ -25,13 +27,15 @@ def hi(fast_request: FastRequest):
 @app.get("/hello/{framework}")
 def hello(framework):
     url = eureka_client.walk_nodes(framework, "/", walker=lambda a: a)
-    json = {"name": "FastAPI"}
 
-    print(url)
-    response = requests.post(url, json=json)
-    data = response.json()
-    return data
+    data = {"name": "FastAPI"}
+    data = str(json.dumps(data)).encode("utf-8")
+
+    req = request.Request(url, data)  # type: ignore
+
+    response = request.urlopen(req)
+    return json.loads(response.read())
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8001)  # type: ignore
